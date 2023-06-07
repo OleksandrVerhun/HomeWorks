ceef3a591b1bca201a48400d1f7879d388c1b937
Oleksandr_Verhun <oleksandrverhun1@gmail.com>
2023-06-07 18:55:58 +0300

    Add project files.

---
+++ HomeWork3/BlackJack.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    public class BlackJack
+    {
+        private int wins = 0, loses = 0, draws = 0;
+
+        private Deck deck;
+        private List<Card> originalDeck;
+        private List<Card> playerCards;
+        private List<Card> computerCards;
+        private List<Card> dealerCards;
+
+        public BlackJack(Deck deck)
+        {
+            this.deck = deck;
+            originalDeck = deck.GetCards();
+            playerCards = new List<Card>();
+            computerCards = new List<Card>();
+            dealerCards = deck.GetCards();
+        }
+
+        private void DealInitialCards()
+        {
+            playerCards.Add(dealerCards[0]);
+            dealerCards.RemoveAt(0);
+            playerCards.Add(dealerCards[0]);
+            dealerCards.RemoveAt(0);
+
+            computerCards.Add(dealerCards[0]);
+            dealerCards.RemoveAt(0);
+            computerCards.Add(dealerCards[0]);
+            dealerCards.RemoveAt(0);
+        }
+
+        public void StartGame()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                playerCards.Clear();
+                computerCards.Clear();
+                dealerCards = new List<Card>(originalDeck);
+                deck.Shuffle(dealerCards);
+                DealInitialCards();
+
+                bool gameOver = false;
+                while (!gameOver)
+                {
+                    Console.Clear();
+
+                    if (!IsGameEnded())
+                    {
+                        if (!PlayerTurn())
+                            gameOver = true;
+
+                        ComputerTurn();
+                    }
+                    else
+                        gameOver = true;
+                }
+
+                GameResults();
+
+                int choice;
+                do
+                {
+                    Console.WriteLine("\n1) Continue playing 2) Exit");
+                } while (!int.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2));
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        return;
+                }
+            }
+        }
+
+        private bool PlayerTurn()
+        {
+            Console.WriteLine("Your Cards:");
+            DisplayHand(playerCards);
+            int option;
+            do
+            {
+                Console.WriteLine("\n1) Take 2) Stop");
+            } while (!int.TryParse(Console.ReadLine(), out option) || (option != 1 && option != 2));
+
+            bool continuePlaying = true;
+
+            switch (option)
+            {
+                case 1:
+                    playerCards.Add(dealerCards[0]);
+                    dealerCards.RemoveAt(0);
+                    break;
+                case 2:
+                    continuePlaying = false;
+                    break;
+            }
+
+            return continuePlaying;
+        }
+
+        private void ComputerTurn()
+        {
+            if (GetHandValue(computerCards) < 12)
+            {
+                computerCards.Add(dealerCards[0]);
+                dealerCards.RemoveAt(0);
+            }    
+        }
+
+        private bool IsGameEnded()
+        {
+            bool flag;
+
+            if (GetHandValue(computerCards) >= 21 || GetHandValue(playerCards) >= 21 || HasTwoAces(computerCards) || HasTwoAces(playerCards))
+                flag = true;
+            else
+                flag = false;
+
+            return flag;
+        }
+
+        private void GameResults()
+        {
+            Console.Clear();
+            Console.WriteLine("Your Cards:");
+            DisplayHand(playerCards);
+            Console.WriteLine("\nComputer Cards:");
+            DisplayHand(computerCards);
+
+            int playerValue = GetHandValue(playerCards);
+            int computerValue = GetHandValue(computerCards);
+
+            if (playerValue == 21 || HasTwoAces(playerCards))
+            {
+                Console.WriteLine("\n - = You Win = -\n");
+                wins++;
+            }
+            else if (computerValue == 21 || HasTwoAces(computerCards))
+            {
+                Console.WriteLine("\n - = Computer Wins = -\n");
+                loses++;
+            }
+            else if (playerValue > 21)
+            {
+                Console.WriteLine("\n - = Computer Wins = -\n");
+                loses++;
+            }
+            else if (computerValue > 21)
+            {
+                Console.WriteLine("\n - = You Win = -\n");
+                wins++;
+            }
+            else if (playerValue > computerValue)
+            {
+                Console.WriteLine("\n - = You Win = -\n");
+                wins++;
+            }
+            else if (playerValue < computerValue)
+            {
+                Console.WriteLine("\n - = Computer Wins = -\n");
+                loses++;
+            }
+            else
+            {
+                Console.WriteLine("\n - = Draw = -\n");
+                draws++;
+            }
+
+            Console.WriteLine($"Wins: {wins} | Loses: {loses} | Draws: {draws}");
+        }
+
+        private int GetHandValue(List<Card> hand)
+        {
+            int value = 0;
+
+            foreach (var item in hand)
+            {
+                if (item.Value == Value.Six)
+                    value += 6;
+                else if (item.Value == Value.Seven)
+                    value += 7;
+                else if (item.Value == Value.Eight)
+                    value += 8;
+                else if (item.Value == Value.Nine)
+                    value += 9;
+                else if (item.Value == Value.Ten)
+                    value += 10;
+                else if (item.Value == Value.Jack)
+                    value += 2;
+                else if (item.Value == Value.Queen)
+                    value += 3;
+                else if (item.Value == Value.King)
+                    value += 4;
+                else if (item.Value == Value.Ace)
+                    value += 11;
+            }
+
+            return value;
+        }
+
+        private void DisplayHand(List<Card> hand)
+        {
+            foreach (var item in hand)
+                Console.WriteLine($"{item.Value} {item.Suit}");
+            Console.WriteLine($"\nTotal hand value: {GetHandValue(hand)}");
+        }
+
+        private bool HasTwoAces(List<Card> hand)
+        {
+            int numAce = 0;
+            bool flag = false;
+
+            foreach (var item in hand)
+            {
+                if (item.Value == Value.Ace)
+                    numAce++;
+            }
+
+            if (hand.Count == 2 && numAce == 2)
+                flag = true;
+
+            return flag;
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,218 @@
+﻿//1) Згенерувати впорядковану колоду карт
+//2) Перемішати колоду карт
+//3) Знайти позиції всіх тузів у колоді
+//4) Перемістити всі пікові карти на початок колоди
+//5) Відсортувати колоду
+
+//6) Створіть консольну програму для карткової гри «21» з простими правилами:
+//a. у грі 36 карт
+//b. вартість карток в балах: Туз - 11 очок, Король – 4 очки, Дама/Королева - 3 бали, Джек/Валет – 2 очки, інші карти за їх номіналом
+//c. 2 гравці (ви та комп'ютер)
+//d. мета гри - набрати 21 очко
+//e. спочатку ви повинні ввести, хто отримує перші картки
+//f. ви та комп'ютер отримуєте 2 карти відразу
+//g. після цього кожен із вас вирішить, чого ви хочете? отримати ще одну карту або перестати
+//   отримувати карти (залежить від того, хто першим почав гру)
+//h. якщо гравець набрав 21 очко або має 2 тузи, гра закінчена і виграє цей гравенць
+//i. якщо гравець набирає більше 21 очка, гра закінчується, але в кінці раунду
+//j. якщо у вас обох більше 21 очка гра закінчена та виграє гравець з меншою кількістю очок
+//k. має бути можливість продовжувати грати
+//l. статистика за результатами всіх зіграних ігор
+
+using System;
+
+namespace HomeWork3;
+
+public enum Suit
+{
+    Spades, // ♠
+    Hearts, // ♥
+    Clubs, // ♣
+    Diamonds // ♦
+}
+
+public enum Value
+{
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
+public class Card
+{
+    public Suit Suit { get; set; }
+    public Value Value { get; set; }
+}
+
+public class Deck
+{
+    public List<Card> cards { get; set; }
+
+    public Deck()
+    {
+        cards = new List<Card>();
+        GenerateDeck();
+    }
+
+    private void GenerateDeck()
+    {
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+        {
+            foreach (Value value in Enum.GetValues(typeof(Value)))
+            {
+                cards.Add(new Card { Suit = suit, Value = value });
+            }
+        }
+    }
+
+    public List<Card> GetCards() => cards;
+
+    public void Shuffle(List<Card> cards)
+    {
+        Random random = new Random();
+        int n = cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            Card card = cards[k];
+            cards[k] = cards[n];
+            cards[n] = card;
+        }
+    }
+
+    public void PrintDeck()
+    {
+        int i = 0;
+        foreach (var item in cards)
+        {
+            Console.WriteLine($"{i}) {item.Value} {item.Suit}");
+            i++;
+        }
+    }
+
+    public void SortDeck()
+    {
+        cards.Sort((card1, card2) =>
+        {
+            int suitComparison = card1.Suit.CompareTo(card2.Suit);
+            if (suitComparison == 0)
+            {
+                return card1.Value.CompareTo(card2.Value);
+            }
+            else
+            {
+                return suitComparison;
+            }
+        });
+    }
+
+    public List<int> GetAcePositions()
+    {
+        List<int> acePositions = new List<int>();
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            if (cards[i].Value == Value.Ace)
+            {
+                acePositions.Add(i);
+            }
+        }
+
+        return acePositions;
+    }
+
+    public void MoveSpadesToBeginning()
+    {
+        List<Card> spades = new List<Card>();
+        List<Card> otherCards = new List<Card>();
+
+        foreach (var card in cards)
+        {
+            if (card.Suit == Suit.Spades)
+                spades.Add(card);
+            else
+                otherCards.Add(card);
+        }
+
+        cards.Clear();
+        cards.AddRange(spades);
+        cards.AddRange(otherCards);
+    }
+}
+
+public class Program
+{
+    public static void Menu()
+    {
+        bool flag = true;
+        while (flag)
+        {
+            try
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("1) Start new game\n2) Inspection of methods\n0) Exit ");
+                Console.Write("\nSelect the option: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+                switch (item)
+                {
+                    case 1:
+                        Deck deck = new Deck();
+                        BlackJack jack = new BlackJack(deck);
+                        jack.StartGame();
+                        break;
+                    case 2: // Зробив для цього окремий кейс, тому що НЕ всі методи використовуються у грі
+                        Console.Clear();
+                        Console.WriteLine("Inspection of methods");
+
+                        Console.WriteLine("\nCreating a deck and displaying all cards");
+                        Deck testDeck = new Deck();
+                        testDeck.PrintDeck();
+
+                        Console.WriteLine("\nShuffle the deck");
+                        testDeck.Shuffle(testDeck.cards);
+                        testDeck.PrintDeck();
+
+                        Console.WriteLine("\nFind ace positions");
+                        foreach (var acePos in testDeck.GetAcePositions())
+                        {
+                            Console.Write($"{acePos} ");
+                        }
+
+                        Console.WriteLine("\n\nMove all spade cards to the beginning of the deck");
+                        testDeck.MoveSpadesToBeginning();
+                        testDeck.PrintDeck();
+
+                        Console.WriteLine("\nDeck sorting");
+                        testDeck.SortDeck();
+                        testDeck.PrintDeck();
+
+                        Console.WriteLine("\nPress any button to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        Console.WriteLine("\nThank you for playing!");
+                        return;
+                    default:
+                        throw new Exception("Incorrect input! Try again...");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        Menu();
+    }
+}
